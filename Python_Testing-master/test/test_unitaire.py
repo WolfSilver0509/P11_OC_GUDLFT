7ec86510ff9c86c2e73066c167d9bdf8a3fd1c6b
Denizot <denizot.myriam@gmail.com>
2023-08-25 00:26:13 +0200

    bug not more than 12 places per competition club + test

--- Python_Testing-master/test/test_unitaire.py
+++ Python_Testing-master/test/test_unitaire.py
@@ -1,6 +1,8 @@
 from server import app
 import html
 import json
+from .conftest import client, mock_competitions, mock_clubs
+
 
 def test_show_summary_with_existing_email(monkeypatch):
     # Définir les données simulées pour la requête
@@ -62,3 +64,26 @@ def test_purchase_places_with_not_enough_points_available(client):
     # Vérifier la réponse
     assert response.status_code == 200
     assert "Not enough points available for this club." in response.data.decode('utf-8')
+
+def test_purchase_places_exceed_max_limit(client , mock_competitions, mock_clubs):
+    # charger des données de test pour la compétition et le club pour tester la réservation de places
+    competitions = mock_competitions
+    clubs = mock_clubs
+
+    # Simulez une requête POST avec un nombre de places supérieur à la limite maximale
+    response = client.post('/purchasePlaces', data={ 'club': 'Simply_Lift','competition': 'More_than_12_places_avalaible', 'places': '13'})
+
+    # Vérifiez la réponse et le message flash correspondant
+    assert response.status_code == 200
+    assert "Vous ne pouvez pas réserver plus de 12 places / You cannot book more than 12 places." in response.data.decode('utf-8')
+
+
+def test_purchase_valid_places(client, mock_competitions, mock_clubs):
+    # Créez des données de test pour la compétition et le club pour tester la réservation de places
+    competition = mock_competitions
+    club = mock_clubs
+    # Simulez une requête POST avec un nombre de places dans la limite maximale
+    response = client.post('/purchasePlaces', data={'competition': 'More_than_12_places_avalaible', 'club': 'Simply_Lift', 'places': '6'})
+    # Vérifiez la réponse et le message flash correspondant
+    assert response.status_code == 200
+    assert "Super votre réservation est bien prise en compte / Great-booking complete!" in response.data.decode('utf-8')
